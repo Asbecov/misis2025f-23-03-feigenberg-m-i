610647ddb9b610203fe3423915108b8f2f018453
Asbecov <m2311505@edu.misis.ru>
2025-10-04 22:50:18 +0300

    initial

---
+++ prj.lab/lab01/task01-02.cpp
@@ -0,0 +1,3 @@
+#include <semcv>
+
+int main() {}
---
+++ prj.lab/semcv/semcv.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef SEMCV
+#define SEMCV
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#include <opencv2/core/mat.hpp>
+
+std::string strid_from_mat(const cv::Mat& img, const int n = 4);
+
+std::vector<std::filesystem::path> get_list_of_file_paths(const std::filesystem::path& path_lst);
+
+cv::Mat gen_tgtimg00(const int lev0, const int lev1, const int lev2);
+
+#endif
